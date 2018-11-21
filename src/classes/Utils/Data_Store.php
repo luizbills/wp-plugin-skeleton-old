bc038c919689db71551cb8b921e069d511fc8531
Luiz Paulo "Bills <luizpbills@gmail.com>
2018-11-21 19:30:01 -0200

    Create Data_Store.php

---
+++ src/classes/Utils/Data_Store.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace {{namespace}};
+
+class Data_Store {
+	protected $data = [];
+
+	public function __construct ( $_values = [] ) {
+		$values = array_merge( $this->get_defaults(), $_values );
+
+		foreach ( $values as $key => $value) {
+			$this->set( $key, $value );
+		}
+
+		$this->init();
+	}
+
+	public function set ( $key, $value ) {
+		$this->data[ $key ] = $value;
+	}
+
+	public function get ( $key ) {
+		return isset( $this->data[ $key ] ) ? $this->data[ $key ] : '';
+	}
+
+	public function has ( $key ) {
+		return isset( $this->data[ $key ] );
+	}
+
+	public function delete ( $key ) {
+		if ( isset( $this->data[ $key ] ) ) {
+			unset ( $this->data[ $key ] );
+		}
+	}
+
+	public function get_defaults () {
+		return [];
+	}
+}
