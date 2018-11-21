8518e39b38a618fac20b648cafd76dea2bda1fed
Luiz Paulo "Bills <luizpbills@gmail.com>
2018-11-21 19:37:10 -0200

    Create Immutable_Data_Store.php

---
+++ src/classes/Utils/Immutable_Data_Store.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace {{namespace}};
+
+use {{namespace}}\Utils\Data_Store;
+
+class Immutable_Data_Store extends Data_Store {
+
+	public function set ( $key, $value ) {
+		if ( $this->has( $key ) ) {
+			throw new \Exception( "key \"$key\" already assigned" );
+		}
+		$this->data[ $key ] = $value;
+	}
+	
+	public function delete ( $key ) {
+		throw new \Exception( 'Can not delete keys' );
+	}
+}
