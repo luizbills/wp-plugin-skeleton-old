2f1dd3bdfe982d1c4f2dbd7e34026741d4e01153
Luiz Paulo "Bills <luizpbills@gmail.com>
2018-11-21 20:48:13 -0200

    Update Config.php

--- src/classes/Config.php
+++ src/classes/Config.php
@@ -7,41 +7,31 @@ use {{namespace}}\Utils\Immutable_Data_Store;
 use Symfony\Component\Yaml\Yaml;
 
 class Config extends Immutable_Data_Store {
-	
+
 	protected static $instance = null;
-	
-	protected function get_instance () {
+
+	public function get_instance () {
 		if ( is_null( self::$instance ) ) {
 			self::$instance = new self();
 		}
 		return self::$instance;
 	}
-	
+
 	public static function initialize ( $PLUGIN_FILE ) {
+		$config = self::get_instance();
+
 		$plugin_config = Yaml::parseFile( dirname( $PLUGIN_FILE ) . '/plugin.yml' );
 		$plugin_name = $plugin_config['plugin_name'];
 		$plugin_slug = slugify( $plugin_name );
 		$plugin_prefix = \str_replace( '-', '_', $plugin_slug ) . '_';
 
-		self::set( 'PLUGIN_NAME', $plugin_name );
-		self::set( 'SLUG', $plugin_slug );
-		self::set( 'PREFIX', $plugin_prefix );
-		self::set( 'VERSION', $plugin_config['version'] );
-		self::set( 'ASSETS_DIR', $plugin_config['assets_dir'] );
-		self::set( 'TEMPLATES_DIR', $plugin_config['templates_dir'] );
-		self::set( 'FILE', $PLUGIN_FILE );
-		self::set( 'DIR', dirname( $PLUGIN_FILE ) );
-	}
-	
-	public static function set ( $key, $value ) {
-		self::get_instance()->set( $key, $value );
-		return $value;
-	}
-	
-	public static function get ( $key, $default = null ) {
-		if ( self::get_instance()->has( $key ) ) {
-			return self::get_instance()->get( $key )
-		}
-		return $default;
+		$config->set( 'PLUGIN_NAME', $plugin_name );
+		$config->set( 'SLUG', $plugin_slug );
+		$config->set( 'PREFIX', $plugin_prefix );
+		$config->set( 'VERSION', $plugin_config['version'] );
+		$config->set( 'ASSETS_DIR', $plugin_config['assets_dir'] );
+		$config->set( 'TEMPLATES_DIR', $plugin_config['templates_dir'] );
+		$config->set( 'FILE', $PLUGIN_FILE );
+		$config->set( 'DIR', dirname( $PLUGIN_FILE ) );
 	}
 }
