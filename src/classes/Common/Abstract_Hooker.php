7997ac7e5742dfd8348a7ae66af77865e5335fde
Luiz Paulo "Bills <luizbills@pm.me>
2018-11-25 23:09:17 -0200

    Update Abstract_Hooker.php

--- src/classes/Common/Abstract_Hooker.php
+++ src/classes/Common/Abstract_Hooker.php
@@ -4,11 +4,8 @@ namespace {{namespace}}\Common;
 
 abstract class Abstract_Hooker {
 
-	protected function call_hook_function ( $hook_function, $hook, $callback, $priority = 10, $arguments = 1 ) {
-		if ( ! is_callable( $callback ) ) {
-			$callback = [ $this, $callback ];
-		}
-		$hook_function( $hook, $callback, $priority, $arguments );
+	protected function call_hook_function ( $hook_function, $hook, $callback, $priority, $arguments ) {
+		$hook_function( $hook, [ $this, $callback ], $priority, $arguments );
 	}
 
 	public function add_action ( $hook, $callback, $priority = 10, $arguments = 1 ) {
