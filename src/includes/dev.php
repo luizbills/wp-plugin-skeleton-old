1d63390fe3e6a82a9d8b8a8be62fbdf6486f2c8b
Luiz Bills <luizpbills@gmail.com>
2018-11-29 20:28:40 +0000

    ignore dev.php

--- src/includes/dev.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace {{namespace}};
-
-use function {{namespace}}\functions\log_debug;
-use {{namespace}}\Core\Config;
-
-log_debug( 'Testing... works?' );
-log_debug( Config::get_options() );
