055dd922055ec2dc0ac7aa1c621e0f3ef2976912
Luiz Paulo "Bills <luizpbills@gmail.com>
2018-12-06 17:49:34 -0200

    Update hooks.php

--- src/includes/hooks.php
+++ src/includes/hooks.php
@@ -9,12 +9,8 @@ use {{namespace}}\Core\Config;
 $demo = Config::set( '$demo', new Demo() );
 $demo->add_action( 'admin_notices', 'display_admin_notice' );
 
-// enqueue scripts (js and css)
-if ( WP_DEBUG ) {
-	$script_manager = Config::set( '$script_manager', new Utils\Script_Manager() );
-
-	$script_manager->add_global_script_dependency( 'jquery' );
-	$script_manager->enqueue_script( get_asset_url( 'js/demo.js' ) );
-
-	$script_manager->enqueue_style( get_asset_url( 'css/demo.css' ) );
-}
+// another example: how to enqueue static scripts (javascript and css)
+$script_manager = Config::set( '$script_manager', new Utils\Script_Manager() );
+$script_manager->add_global_script_dependency( 'jquery' );
+$script_manager->enqueue_script( get_asset_url( 'js/demo.js' ) );
+$script_manager->enqueue_style( get_asset_url( 'css/demo.css' ) );
