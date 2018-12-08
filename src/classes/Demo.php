56c934807cb79758af386d0960e5d8c338a48279
Luiz Paulo "Bills <luizpbills@gmail.com>
2018-12-07 23:40:10 -0200

    Update Demo.php

--- src/classes/Demo.php
+++ src/classes/Demo.php
@@ -3,14 +3,14 @@
 namespace {{namespace}};
 
 use {{namespace}}\Core\Config;
-use function {{namespace}}\functions\include_template_file;
+use function {{namespace}}\functions\include_template;
 
 class Demo extends Common\Abstract_Hooker {
 
 	public function display_admin_notice () {
 		$plugin_name = Config::get('PLUGIN_NAME');
 
-		include_template_file( 'admin-notice.php', [
+		include_template( 'admin-notice.php', [
 			'message' => "Plugin $plugin_name activated.",
 			'class' => 'notice-success is-dismissible'
 		] );
