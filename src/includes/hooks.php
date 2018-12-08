f814bb85c9303830fac8af8fc330f7b2c9e56d79
Luiz Paulo "Bills <luizbills@pm.me>
2018-12-08 10:24:43 -0200

    Update hooks.php

--- src/includes/hooks.php
+++ src/includes/hooks.php
@@ -5,15 +5,16 @@
 
 namespace {{namespace}};
 
+use function {{namespace}}\functions\config_set;
 use function {{namespace}}\functions\get_asset_url;
-use {{namespace}}\Core\Config;
+use {{namespace}}\Utils\Script_Manager;
 
 // just an example
-$demo = Config::set( '$demo', new Demo() );
+$demo = config_set( '$demo', new Demo() );
 $demo->add_action( 'admin_notices', 'display_admin_notice' );
 
 // another example: how to enqueue static scripts (javascript and css)
-$script_manager = Config::set( '$script_manager', new Utils\Script_Manager() );
+$script_manager = config_set( '$script_manager', new Script_Manager() );
 $script_manager->add_global_script_dependency( 'jquery' );
 $script_manager->enqueue_script( get_asset_url( 'js/demo.js' ) );
 $script_manager->enqueue_style( get_asset_url( 'css/demo.css' ) );
