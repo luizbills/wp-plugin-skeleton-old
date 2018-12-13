00f96474a02ad173db71319d48c7502a4464a698
Luiz Paulo "Bills <luizbills@pm.me>
2018-12-13 10:39:45 -0200

    Update post.php

--- src/includes/helpers/post.php
+++ src/includes/helpers/post.php
@@ -6,7 +6,7 @@
 namespace {{namespace}}\functions;
 
 function get_post ( $id, $post_type = 'post' ) {
-	$post = \get_post( $id );
+	$post = \get_post( \intval( $id ) );
 	if ( $post && $post_type === $post->post_type ) {
 		return $post;
 	}
