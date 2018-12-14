df573b6bc48dbc500fc10b28bade4cc08c95ba30
Luiz Paulo "Bills <luizpbills@gmail.com>
2018-12-14 13:07:44 -0200

    Create array.php

---
+++ src/includes/helpers/array.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @version 1.0.0
+ */
+
+namespace {{namespace}}\functions;
+
+function array_unset_keys ( &$arr, $keys ) {
+	foreach ( $keys as $key ) {
+		unset( $arr[ $key ] );
+	}
+}
+
+function array_extract_key ( &$arr, $key ) {
+	$value = null;
+	if ( ! isset( $arr[ $key ] ) ) {
+		$value = $arr[ $key ];
+		unset( $arr[ $key ] );
+	}
+	return $value;
+}
