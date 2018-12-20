a48668d1cd84e270ba2b20461bdf305021a26850
Luiz Bills <luizpbills@gmail.com>
2018-12-20 19:55:27 +0000

    fix namespace

--- src/includes/helpers.php
+++ src/includes/helpers.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version 1.1.0
+ * @version 1.3.0
  */
 
 require_once 'helpers/config.php';
--- src/includes/helpers/string.php
+++ src/includes/helpers/string.php
@@ -3,39 +3,39 @@
  * @version 1.0.0
  */
 
-use {{namespace}}\functions;
+namespace {{namespace}}\functions;
 
-function str_after ( $subject, $search ) {
-	return '' === $search ? $subject : \array_reverse( \explode( $search, $subject, 2 ) )[0];
+function str_after ( $string, $search ) {
+	return '' === $search ? $string : \array_reverse( \explode( $search, $string, 2 ) )[0];
 }
 
-function str_before ( $subject, $search ) {
-	return '' === $search ? $subject : \explode( $search, $subject )[0];
+function str_before ( $string, $search ) {
+	return '' === $search ? $string : \explode( $search, $string )[0];
 }
 
-function str_length ( $value, $encoding = null ) {
+function str_length ( $string, $encoding = null ) {
 	if ( null !== $encoding ) {
-		return \mb_strlen( $value, $encoding );
+		return \mb_strlen( $string, $encoding );
 	}
-	return \mb_strlen( $value );
+	return \mb_strlen( $string );
 }
 
-function str_lower ( $value ) {
-	return \mb_strtolower( $value, 'UTF-8' );
+function str_lower ( $string ) {
+	return \mb_strtolower( $string, 'UTF-8' );
 }
 
-function str_upper ( $value ) {
-	return \mb_strtoupper( $value, 'UTF-8' );
+function str_upper ( $string ) {
+	return \mb_strtoupper( $string, 'UTF-8' );
 }
 
-function str_slug ( $value, $separator = '-' ) {
+function str_slug ( $string, $separator = '-' ) {
 	// TO DO
 }
 
-function str_starts_with ( $haystack, $needle ) {
-	return \substr( $haystack, 0, \strlen( $needle ) ) === $needle;
+function str_starts_with ( $string, $search ) {
+	return \substr( $string, 0, \strlen( $search ) ) === $search;
 }
 
-function str_ends_with ( $haystack, $needle ) {
-	return \substr( $haystack, -\strlen( $needle ) ) === $needle;
+function str_ends_with ( $string, $search ) {
+	return \substr( $string, -\strlen( $search ) ) === $search;
 }
