7e187030172a6849171ece3719e6f6a9fec60a67
Luiz Paulo "Bills <luizpbills@gmail.com>
2018-12-19 22:05:50 -0200

    Update array.php

--- src/includes/helpers/array.php
+++ src/includes/helpers/array.php
@@ -5,21 +5,6 @@
 
 namespace {{namespace}}\functions;
 
-function array_unset_keys ( &$arr, $keys ) {
-	foreach ( $keys as $key ) {
-		unset( $arr[ $key ] );
-	}
-}
-
-function array_extract_key ( &$arr, $key ) {
-	$value = null;
-	if ( isset( $arr[ $key ] ) ) {
-		$value = $arr[ $key ];
-		unset( $arr[ $key ] );
-	}
-	return $value;
-}
-
 function array_head ( $arr ) {
 	return $arr[0]
 }
@@ -30,4 +15,38 @@ function array_tail ( $arr ) {
 
 function array_divide ( $arr ) {
 	return [ \array_keys( $arr ), \array_values( $arr ) ];
+}
+
+function array_get ( $arr, $key, $default = null ) {
+	$value = $default;
+	if ( isset( $arr[ $key ] ) ) {
+		$value = $arr[ $key ];
+	}
+	return $value;
+}
+
+function array_pull ( &$arr, $key, $default = null ) {
+	$value = array_get( $arr, $key, $default );
+	unset( $arr[ $key ] );
+	return $value;
+}
+
+function array_forget ( &$arr, $keys ) {
+	foreach ( (array) $keys as $key ) {
+		if ( isset( $arr[ $key ] ) ) {
+			unset( $arr[ $key ] );
+		}
+	}
+	return $arr;
+}
+
+function array_only ( &$arr, $keys ) {
+    return \array_intersect_key( $arr, \array_flip( (array) $keys ) );
+}
+
+function wrap ( $value ) {
+	if ( is_null( $value ) ) {
+	    return [];
+	}
+	return is_array($value) ? $value : [ $value ];
 }
