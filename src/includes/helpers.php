8293720e3c1a6f18ffc9276e205a4ec33108766b
Luiz Paulo "Bills <luizpbills@gmail.com>
2018-12-19 22:17:02 -0200

    Update helpers.php

--- src/includes/helpers.php
+++ src/includes/helpers.php
@@ -11,5 +11,6 @@ require_once 'helpers/file.php';
 require_once 'helpers/logger.php';
 require_once 'helpers/post.php';
 require_once 'helpers/slug.php';
+require_once 'helpers/string.php';
 require_once 'helpers/template.php';
 require_once 'helpers/user.php';
