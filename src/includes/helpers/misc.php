0bba86655a51b39ca8d9029803693e1e0b3c6ea7
Luiz Paulo "Bills <luizbills@pm.me>
2019-01-09 16:13:41 -0200

    Update misc.php

--- src/includes/helpers/misc.php
+++ src/includes/helpers/misc.php
@@ -10,6 +10,8 @@ function value ( $value, $default = '' ) {
 	return empty( $result ) ? $default : $result;
 }
 
+// returns a value of a global array if it exists or an empty string
+// example: request_value( 'PATH', 'server' ) returns $_SERVER['PATH']
 function request_value ( $key, $type = 'get' ) {
 	$array = '_' . strtoupper( $type );
 	return isset( $GLOBALS[ $array ] ) && isset( $GLOBALS[ $array ][ $key ] ) ? $GLOBALS[ $array ][ $key ] : '';
