772bdf9bb818264a608d7cbcfa3c9058675c0eaf
Luiz Paulo "Bills <luizbills@pm.me>
2019-01-09 16:12:11 -0200

    add request_value

--- src/includes/helpers/misc.php
+++ src/includes/helpers/misc.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version 1.0.2
+ * @version 1.1.0
  */
 
 namespace {{namespace}}\functions;
@@ -8,6 +8,11 @@ namespace {{namespace}}\functions;
 function value ( $value, $default = '' ) {
 	$result = is_callable( $value ) ? call_user_func( $value ) : $value;
 	return empty( $result ) ? $default : $result;
+}
+
+function request_value ( $key, $type = 'get' ) {
+	$array = '_' . strtoupper( $type );
+	return isset( $GLOBALS[ $array ] ) && isset( $GLOBALS[ $array ][ $key ] ) ? $GLOBALS[ $array ][ $key ] : '';
 }
 
 function throw_if ( $condition, $exception, ...$parameters ) {
