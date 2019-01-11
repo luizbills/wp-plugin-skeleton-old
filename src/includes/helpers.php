cda4161f41b5e0f24771e95e7eac813a26972e72
Luiz Paulo "Bills <luizbills@pm.me>
2019-01-10 22:49:01 -0200

    Update helpers.php

--- src/includes/helpers.php
+++ src/includes/helpers.php
@@ -1,15 +1,14 @@
 <?php
 /**
- * @version 1.3.0
+ * @version 1.3.1
  */
-
-require_once 'helpers/config.php';
-require_once 'helpers/misc.php';
 
 require_once 'helpers/array.php';
 require_once 'helpers/cache.php';
+require_once 'helpers/config.php';
 require_once 'helpers/file.php';
 require_once 'helpers/logger.php';
+require_once 'helpers/misc.php';
 require_once 'helpers/post.php';
 require_once 'helpers/slug.php';
 require_once 'helpers/string.php';
