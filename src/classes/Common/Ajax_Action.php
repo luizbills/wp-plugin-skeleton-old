e19360293f7540b340ea5b4d808a90075f81e26a
Luiz Paulo "Bills <luizbills@pm.me>
2019-01-12 13:49:40 -0200

    Create Ajax_Action.php

---
+++ src/classes/Common/Ajax_Action.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @version 1.0.0
+ */
+
+namespace {{namespace}}\Common;
+
+abstract class Ajax_Action extends Abstract_Hooker {
+	public function add_hooks () {
+		$this->add_action( 'wp_ajax_' . $this->get_action_name(), 'callback' );
+		
+		if ( $this->is_public() ) {
+			$this->add_action( 'wp_ajax_nopriv_' . $this->get_action_name(), 'callback' );
+		}
+	}
+
+	public function callback () {
+		// this should be implemented
+		\wp_send_json_error(
+			[
+				'error' => 'Callback not implemented'
+			],
+			400
+		);
+	}
+	
+	public function get_action_name () {
+		// this should be implemented
+		return '';
+	}
+	
+	public function is_public () {
+		return false;
+	}
+}
