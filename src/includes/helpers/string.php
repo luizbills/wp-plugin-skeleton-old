46dd37c39f5525bac30d3ee38b632e7eeef27afd
Luiz Paulo "Bills <luizbills@pm.me>
2019-02-21 09:59:02 -0300

    Update string.php

--- src/includes/helpers/string.php
+++ src/includes/helpers/string.php
@@ -28,8 +28,28 @@ function str_upper ( $string ) {
 	return \mb_strtoupper( $string, 'UTF-8' );
 }
 
-function str_slug ( $string, $separator = '-' ) {
-	// TO DO
+function str_slug ( $text, $separator = '-' ) {
+	$slug = \remove_accents( $text ); // Convert to ASCII
+
+	$find = [
+		' ',
+		'_',
+		'-',
+	];
+	$replace = $separator;
+
+	// Standard replacements
+	$slug = \str_replace( $find, $replace, $slug );
+	// Remove all non-alphanumeric except $separator
+	$slug = \preg_replace( "/[^A-Za-z0-9\\$separator]/", '', $slug );
+	// Replace any more than one $separator in a row
+	$slug = \preg_replace( "/\\$separator+/", $separator, $slug );
+	// Remove last $separator if at the end
+	$slug = \preg_replace( "/\\$separator\\$/", '', $slug );
+	// Lowercase
+	$slug = \strtolower( $slug );
+
+	return $slug;
 }
 
 function str_starts_with ( $string, $search ) {
