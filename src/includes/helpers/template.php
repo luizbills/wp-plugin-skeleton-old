9e2e72610885a6275bfb930488a06e1690a866c1
Luiz Bills <luizpbills@gmail.com>
2019-03-01 17:11:20 -0300

    add include_php_template helper

--- src/includes/helpers/template.php
+++ src/includes/helpers/template.php
@@ -1,20 +1,32 @@
 <?php
+
+namespace {{namespace}}\functions;
+
+/**
+ * A helper to include PHP files with custom data
+ */
+function include_php_template ( $template_path, $data = [] ) {
+	$template_base_path = config_get( 'ROOT_DIR' ) . '/' . config_get( 'TEMPLATES_DIR' );
+	$template_path = "$template_base_path/$template_path";
+	$data = apply_filters( prefix( 'template_default_data' ), $data );
+
+	extract( $data, \EXTR_OVERWRITE );
+	include $template_path;
+}
+
 /**
  * An safe and simple template engine (only replacement of variables
  * use {{ var }} to safe/scaped values and {{ !var }} to non-scaped values.
  *
  * see: https://github.com/luizbills/wp-plugin-skeleton/blob/master/src/templates/admin-notice.php
  */
-
-namespace {{namespace}}\functions;
-
 function include_template ( $template_path, $data = [] ) {
 	render_template( $template_path, $data, true );
 }
 
 function render_template ( $template_path, $data = [], $echo = false ) {
 	$template_base_path = config_get( 'ROOT_DIR' ) . '/' . config_get( 'TEMPLATES_DIR' );
-	$template_path =  "$template_base_path/$template_path";
+	$template_path = "$template_base_path/$template_path";
 	$template_string = \file_get_contents( $template_path );
 	$data = apply_filters( prefix( 'template_default_data' ), $data );
 	$result = render_template_string( $template_string, $data );
