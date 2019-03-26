68daf371c58aa0556880b3550aadd6583f8d1004
Luiz Paulo "Bills <luizbills@pm.me>
2019-03-26 14:26:15 -0300

    Update Hooker_Trait.php

--- src/classes/Common/Hooker_Trait.php
+++ src/classes/Common/Hooker_Trait.php
@@ -4,23 +4,19 @@ namespace {{namespace}}\Common;
 
 trait Hooker_Trait {
 
-	protected function call_hook_function ( $hook_function, $hook, $callback, $priority, $arguments ) {
-		$hook_function( $hook, [ $this, $callback ], $priority, $arguments );
+	public function add_action ( $hook_name, $callback, $priority = 10, $arguments = 1 ) {
+		return \add_action( $hook_name, [ $this, $callback ], $priority, $arguments );
 	}
 
-	public function add_action ( $hook, $callback, $priority = 10, $arguments = 1 ) {
-		$this->call_hook_function( __FUNCTION__, $hook, $callback, $priority, $arguments );
+	public function remove_action ( $hook_name, $callback, $priority = 10, $arguments = 1 ) {
+		return \remove_action( $hook_name, [ $this, $callback ], $priority, $arguments );
 	}
 
-	public function remove_action ( $hook, $callback, $priority = 10, $arguments = 1 ) {
-		$this->call_hook_function( __FUNCTION__, $hook, $callback, $priority, $arguments );
+	public function add_filter ( $hook_name, $callback, $priority = 10, $arguments = 1 ) {
+		return \add_filter( $hook_name, [ $this, $callback ], $priority, $arguments );
 	}
 
-	public function add_filter ( $hook, $callback, $priority = 10, $arguments = 1 ) {
-		$this->call_hook_function( __FUNCTION__, $hook, $callback, $priority, $arguments );
-	}
-
-	public function remove_filter ( $hook, $callback, $priority = 10, $arguments = 1 ) {
-		$this->call_hook_function( __FUNCTION__, $hook, $callback, $priority, $arguments );
+	public function remove_filter ( $hook_name, $callback, $priority = 10, $arguments = 1 ) {
+		return \remove_filter( $hook_name, [ $this, $callback ], $priority, $arguments );
 	}
 }
