70ef379b75809b5663796febfb00e29f83a7a08d
Luiz Paulo "Bills <luizbills@pm.me>
2019-04-01 12:12:10 -0300

    More secure wp ajax actions
    Add nonces and allowed HTTP methods

--- src/classes/Common/Abstract_Ajax_Action.php
+++ src/classes/Common/Abstract_Ajax_Action.php
@@ -6,19 +6,45 @@ abstract class Abstract_Ajax_Action {
 	use Hooker_Trait;
 
 	public function add_hooks () {
-		$this->add_action( 'wp_ajax_' . $this->get_action_name(), 'callback' );
+		$this->add_action( 'wp_ajax_' . $this->get_action_name(), 'validate_request' );
 
 		if ( $this->is_public() ) {
-			$this->add_action( 'wp_ajax_nopriv_' . $this->get_action_name(), 'callback' );
+			$this->add_action( 'wp_ajax_nopriv_' . $this->get_action_name(), 'validate_request' );
 		}
 	}
 
 	abstract public function get_action_name ();
 
 	abstract public function callback ();
+	
+	public function get_accepted_methods () {
+		return [ 'GET', 'POST' ];
+	}
+	
+	public function get_nonce_action () {
+		return 'ajax_nonce_' . $this->get_action_name();
+	}
+	
+	public function get_nonce_query_arg () {
+		return null; // default is _ajax_nonce or _wpnonce
+	}
 
 	public function is_public () {
 		return false;
+	}
+	
+	public function validate_nonce () {
+		check_ajax_referer( $this->get_nonce_action() );
+	}
+	
+	public function validate_request () {
+		$this->validate_nonce();
+		
+		if ( ! in_array( $_SERVER['REQUEST_METHOD'], $this->get_accepted_methods() ) ) {
+			$this->send_json( [], 405 );
+		}
+		
+		$this->callback();
 	}
 	
 	protected function send_json ( $data, $code = 200 ) {
