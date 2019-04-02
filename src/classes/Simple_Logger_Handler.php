9d7dab2e91c95d13e1ed000dfa45e6744eddba61
Luiz Paulo "Bills <luizbills@pm.me>
2019-04-02 15:34:16 -0300

    Update Simple_Logger_Handler.php

--- src/classes/Simple_Logger_Handler.php
+++ src/classes/Simple_Logger_Handler.php
@@ -9,19 +9,20 @@ class Simple_Logger_Handler {
 	use Hooker_Trait;
 
 	public function add_hooks () {
+		$this->add_filter( h\prefix( 'logger_enabled' ), 'is_enabled', 10, 2 );
 		$this->add_action( h\prefix( 'handle_log' ), 'handle_log', 10, 3 );
 	}
-
-	public function handle_log ( $message, $type, $timestamp ) {
-		if ( ! $this->is_enabled() ) return;
-
-		$type = \strtoupper( $type );
-		$plugin_slug = h\config_get( 'SLUG' );
-
-		\error_log( "$plugin_slug.$type: $message" );
+	
+	public function is_enabled ( $result, $level ) {
+		// check the $level variable to ingore some messages
+		return WP_DEBUG && \defined( 'WP_DEBUG_LOG' ) && WP_DEBUG_LOG;
 	}
 
-	public function is_enabled () {
-		return WP_DEBUG && \defined( 'WP_DEBUG_LOG' ) && WP_DEBUG_LOG;
+	public function handle_log ( $message, $level, $timestamp ) {
+		$level_label = config_get( "LOGGER_LEVEL_$level" );
+		$plugin_slug = h\config_get( 'SLUG' );
+		
+		// most simple wordpress log example
+		\error_log( "$plugin_slug.$level_label: $message" );
 	}
 }
