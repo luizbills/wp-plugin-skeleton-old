669b81c2ab9b2aa32c1603df082d1dcb51c062dc
Luiz Paulo "Bills <luizbills@pm.me>
2019-05-22 13:12:51 -0300

    change validate_request to handle_request

--- src/classes/Common/Abstract_Ajax_Action.php
+++ src/classes/Common/Abstract_Ajax_Action.php
@@ -8,19 +8,15 @@ abstract class Abstract_Ajax_Action {
 	abstract public function get_action_name ();
 
 	public function add_hooks () {
-		$this->add_action( 'wp_ajax_' . $this->get_action_name(), 'validate_request' );
-
+		$this->add_action( 'wp_ajax_' . $this->get_action_name(), 'handle_request' );
 		if ( $this->is_public() ) {
-			$this->add_action( 'wp_ajax_nopriv_' . $this->get_action_name(), 'validate_request' );
+			$this->add_action( 'wp_ajax_nopriv_' . $this->get_action_name(), 'handle_request' );
 		}
 	}
 
-	public function validate_request () {
-		$this->validate_nonce();
-
+	public function handle_request () {
 		$method = $_SERVER['REQUEST_METHOD'];
 		$callback = "handle_$method";
-
 		if ( method_exists( $this,  $callback ) ) {
 			$this->validate_nonce();
 			$this->$callback();
@@ -75,20 +71,17 @@ abstract class Abstract_Ajax_Action {
 
 	protected function send_json ( $data, $error_message = '', $status_code = null ) {
 		$response = [];
-
 		if ( empty( $error_message ) ) {
 			$status_code = $status_code ? $status_code : 200;
 		} else {
 			$status_code = $status_code ? $status_code : 400;
 			$response['error_message'] = $error_message;
 		}
-
 		$response['success'] = $status_code >= 200 && $status_code < 300;
 		$response['data'] = $data;
 		$response['meta'] = [
 			'status_code' => $status_code
 		];
-
 		\wp_send_json( $response, $status_code );
 	}
 }
